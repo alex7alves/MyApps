e34ccd01b90a409d5b78295242303596a0ae0cee
alex7alves <alex7alves10@hotmail.com>
2018-04-16 11:18:05 -0300

    Melhorando texto do resultado

--- Calculadora/app/src/main/java/com/example/alve/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/com/example/alve/calculadora/MainActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
 import java.lang.String;
 public class MainActivity extends AppCompatActivity {
 
     EditText campo1,campo2,resultado;
+    TextView mostrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +23,16 @@ public class MainActivity extends AppCompatActivity {
         campo1 = (EditText)findViewById(R.id.editText);
         campo2 = (EditText)findViewById(R.id.editText2);
         resultado = (EditText)findViewById(R.id.editText3);
-
+        mostrar = (TextView)findViewById(R.id.textView);
+        mostrar.setText("  ");
         campo1.setText("0");
         campo2.setText("0");
     }
+    public void setMostrar(String s){
+        mostrar.setText("O resultado da "+s+ " foi :");
+    }
+
+
     public double getCampo( EditText c){
         String s = String.valueOf(c.getText());
         return Double.parseDouble(s);
@@ -33,18 +42,23 @@ public class MainActivity extends AppCompatActivity {
     }
     public void Somar(View view){
         setResultado(getCampo(campo1)+getCampo(campo2));
+        setMostrar("soma");
     }
     public void Subtrair(View view){
         setResultado(getCampo(campo1)-getCampo(campo2));
+        setMostrar("subtração");
     }
     public void Dividir(View view){
         setResultado(getCampo(campo1)/getCampo(campo2));
+        setMostrar("divisão");
     }
     public void Multiplicar(View view){
         setResultado(getCampo(campo1)*getCampo(campo2));
+        setMostrar("multiplicação");
     }
 
     public void Potencia(View view) {
         setResultado(Math.pow(getCampo(campo1),getCampo(campo2)));
+        setMostrar("potência");
     }
 }
